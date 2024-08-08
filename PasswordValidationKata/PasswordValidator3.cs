61b47a49f1a04a404826c39e3ea10283260de8ed
adrianriverof <adrianriverof@gmail.com>
2024-08-08 13:12:31 +0200

    test: create validator3 tests

---
+++ PasswordValidationKata/PasswordValidator3.cs
@@ -0,0 +1,15 @@
+namespace Tests;
+
+public class PasswordValidator3 {
+    public bool IsValid(string password) {
+        if (password.Length <= 6)
+            return false;
+        if (password.ToLower() == password)
+            return false;
+        if (password.ToUpper() == password)
+            return false;
+        if (!password.Any(Char.IsNumber))
+            return false;
+        return true;
+    }
+}
