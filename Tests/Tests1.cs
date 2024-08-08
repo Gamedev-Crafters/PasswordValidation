2811cdafd575a44f4a9b27387d228c96534cd093
adrianriverof <adrianriverof@gmail.com>
2024-08-08 13:07:07 +0200

    chore: extract passwordvalidator 2 to its own file

--- Tests/Tests1.cs
+++ Tests/Tests1.cs
@@ -36,17 +36,3 @@ public class Tests1 {
 		Assert.IsFalse(sut.IsValid("AHSDFdsapf"));
 	}
 }
-
-public class PasswordValidator2 {
-	public bool IsValid(string password) {
-		if (password.Length <= 6)
-			return false;
-		if (password.ToLower() == password)
-			return false;
-		if (password.ToUpper() == password)
-			return false;
-		if (!password.Any(Char.IsNumber))
-			return false;
-		return true;
-	}
-}
