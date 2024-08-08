04e0f4e25e6f5c0628643fe13c37d9581e6958dd
Gerard Gascón <52170489+GerardGascon@users.noreply.github.com>
2024-08-08 12:34:26 +0200

    Revert "test: IsValid return false if bad password"
    This reverts commit 620f512679bf15e0214d1e8cb9c46220596d4aa4.

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,35 +1,11 @@
 namespace Tests;
 
 public class Tests {
-    
-    // Have more than 8 characters
-    // Contains a capital letter
-    // Contains a lowercase
-    // Contains a number
-    // Contains an underscore
-    
 	[SetUp]
 	public void Setup() { }
 
 	[Test]
-	public void BadPassword()
-    {
-        var sut = new PasswordValidator();
-        Assert.IsFalse(sut.IsValid("hola"));
-    }
-
-    [Test][Ignore("Goal")]
-    public void GoodPassword()
-    {
-        var sut = new PasswordValidator();
-        Assert.IsTrue(sut.IsValid("Hola123_"));
-    }
-}
-
-public class PasswordValidator
-{
-    public bool IsValid(string password)
-    {
-        return false;
-    }
+	public void Test1() {
+		Assert.Pass();
+	}
 }
