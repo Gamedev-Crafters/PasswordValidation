eb17a88c82c9672f5aea372ff478017db309d4cd
JavierStark <javiertorralbocortes@gmail.com>
2024-08-08 13:25:25 +0200

    refactor: extract password class

---
+++ PasswordValidationKata/Password.cs
@@ -0,0 +1,29 @@
+﻿namespace PasswordValidationKata;
+
+public class Password
+{
+    public static bool IsShorterOrEqualTo(string password, int length)
+    {
+        return password.Length <= length;
+    }
+
+    public static bool DoesNotUnderscore(string password)
+    {
+        return !password.Contains('_');
+    }
+
+    public static bool DoesNotNumber(string password)
+    {
+        return !password.Any(Char.IsNumber);
+    }
+
+    public static bool DoesNotLowerCaseLetters(string password)
+    {
+        return password.ToUpper() == password;
+    }
+
+    public static bool DoesNotUpperCaseLetters(string password)
+    {
+        return password.ToLower() == password;
+    }
+}
--- PasswordValidationKata/PasswordValidator1.cs
+++ PasswordValidationKata/PasswordValidator1.cs
@@ -2,41 +2,16 @@
 
 public class PasswordValidator1 {
 	public bool IsValid(string password) {
-		if (IsShorterOrEqualTo(password, 8))
+		if (Password.IsShorterOrEqualTo(password, 8))
 			return false;
-		if (DoesNotUpperCaseLetters(password))
+		if (Password.DoesNotUpperCaseLetters(password))
 			return false;
-		if (DoesNotLowerCaseLetters(password))
+		if (Password.DoesNotLowerCaseLetters(password))
 			return false;
-		if (DoesNotNumber(password))
+		if (Password.DoesNotNumber(password))
 			return false;
-		if (DoesNotUnderscore(password))
+		if (Password.DoesNotUnderscore(password))
 			return false;
 		return true;
-	}
-
-    private static bool IsShorterOrEqualTo(string password, int length)
-    {
-        return password.Length <= length;
-    }
-
-    private static bool DoesNotUnderscore(string password)
-    {
-        return !password.Contains('_');
-    }
-
-    private static bool DoesNotNumber(string password)
-    {
-        return !password.Any(Char.IsNumber);
-    }
-
-    private static bool DoesNotLowerCaseLetters(string password)
-    {
-        return password.ToUpper() == password;
-    }
-
-    private static bool DoesNotUpperCaseLetters(string password)
-	{
-		return password.ToLower() == password;
 	}
 }
