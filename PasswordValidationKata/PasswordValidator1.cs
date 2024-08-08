378f027c6a5924cf40f080bbfb54f0625d06879c
knexator <daniel.hurtadoramirez314@gmail.com>
2024-08-08 13:18:54 +0200

    refactor: extract method

--- PasswordValidationKata/PasswordValidator1.cs
+++ PasswordValidationKata/PasswordValidator1.cs
@@ -4,7 +4,7 @@ public class PasswordValidator1 {
 	public bool IsValid(string password) {
 		if (password.Length <= 8)
 			return false;
-		if (password.ToLower() == password)
+		if (DoesNotUpperCaseLetters(password))
 			return false;
 		if (password.ToUpper() == password)
 			return false;
@@ -13,5 +13,10 @@ public class PasswordValidator1 {
 		if (!password.Contains('_'))
 			return false;
 		return true;
+	}
+
+	private static bool DoesNotUpperCaseLetters(string password)
+	{
+		return password.ToLower() == password;
 	}
 }
