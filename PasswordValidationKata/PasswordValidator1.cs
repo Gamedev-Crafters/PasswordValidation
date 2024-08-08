da0c0b9163b7f7ec7099e97663aca85bed0af520
Gerard Gascón <52170489+GerardGascon@users.noreply.github.com>
2024-08-08 12:58:52 +0200

    chore: moved password validator to another file

---
+++ PasswordValidationKata/PasswordValidator1.cs
@@ -0,0 +1,17 @@
+﻿namespace PasswordValidationKata;
+
+public class PasswordValidator1 {
+	public bool IsValid(string password) {
+		if (password.Length <= 8)
+			return false;
+		if (password.ToLower() == password)
+			return false;
+		if (password.ToUpper() == password)
+			return false;
+		if (!password.Any(Char.IsNumber))
+			return false;
+		if (!password.Contains('_'))
+			return false;
+		return true;
+	}
+}
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using PasswordValidationKata;
+
 namespace Tests;
 
 public class Tests {
@@ -6,55 +8,37 @@ public class Tests {
 
 	[Test]
 	public void GoodPassword() {
-		var sut = new PasswordValidator();
+		var sut = new PasswordValidator1();
 		Assert.IsTrue(sut.IsValid("Hola1234_"));
 	}
 	[Test]
 	public void LessThan8CharShouldFail() {
-		var sut = new PasswordValidator();
+		var sut = new PasswordValidator1();
 		Assert.IsFalse(sut.IsValid("Hola123_"));
 	}
-	
+
 	[Test]
 	public void NoUppercaseShouldFail() {
-		var sut = new PasswordValidator();
+		var sut = new PasswordValidator1();
 		Assert.IsFalse(sut.IsValid("hola1234_"));
 	}
-    
+
     [Test]
     public void NoLowercaseShouldFail() {
-        var sut = new PasswordValidator();
+        var sut = new PasswordValidator1();
         Assert.IsFalse(sut.IsValid("HOLA1234_"));
     }
-	
+
 	[Test]
 	public void NoNumberShouldFail() {
-		var sut = new PasswordValidator();
+		var sut = new PasswordValidator1();
 		Assert.IsFalse(sut.IsValid("Holaholahola_"));
 	}
 
     [Test]
     public void NoUnderscoreShouldFail()
     {
-        var sut = new PasswordValidator();
+        var sut = new PasswordValidator1();
         Assert.IsFalse(sut.IsValid("Hola12347298734289374238947"));
-    }
-    
-    
-}
-
-public class PasswordValidator {
-	public bool IsValid(string password) {
-		if (password.Length <= 8)
-			return false;
-		if (password.ToLower() == password)
-			return false;
-        if (password.ToUpper() == password)
-            return false;
-        if (!password.Any(Char.IsNumber))
-			return false;
-        if (!password.Contains('_')) 
-            return false;
-        return true;
     }
 }
