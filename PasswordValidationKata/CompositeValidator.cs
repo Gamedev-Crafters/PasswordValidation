89ab245d9481d979e1b7b98f7ff24583317d07e0
JavierStark <javiertorralbocortes@gmail.com>
2024-09-01 11:13:51 +0200

    composite validator

---
+++ PasswordValidationKata/CompositeValidator.cs
@@ -0,0 +1,22 @@
+﻿namespace PasswordValidationKata;
+
+public class CompositeValidator : PassValidator
+{
+    private readonly List<PassValidator> validators;
+
+    public CompositeValidator(List<PassValidator> validators)
+    {
+        this.validators = validators;
+    }
+    
+    public Result Validate(Password password)
+    {
+        var reasons = new List<FailureReason>();
+        foreach (var validator in validators)
+        {
+            var result = validator.Validate(password);
+            reasons.AddRange(result);
+        }
+        return new Result(reasons.AsEnumerable());
+    }
+}
